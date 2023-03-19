8c2c4e211c876286529f101dffeb32222bfd8418
p <b>
2023-03-19 15:01:40 -0400

    Corriger les segues dans manage project pour pouvoir se rendre comme il le faut a chaque page

--- Projet IOS H23/ManageProject2ViewController.swift
+++ Projet IOS H23/ManageProject2ViewController.swift
@@ -20,11 +20,9 @@ class ManageProject2ViewController: UIViewController {
     @IBOutlet weak var _totalDepensesLabel: UILabel!
     @IBOutlet weak var _projectNameLabel: UILabel!
     var projectName : String?
-
- 
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if DepenseDAO.shared.getByNomProjet(project_name: projectName!).count == 0 {
+        if DepenseDAO.shared.getByNomProjet(project_name: projectName!).count == 0 && segue.identifier == "VersTableView" {
             let alert = UIAlertController(title: "Aucune dépense", message: "Le projet que vous avez sélectionné n'a encore aucune dépense pour le moment. Essayer d'ajouter une dépense ou revenez plus tard.",         preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "D'accord", style: UIAlertAction.Style.default, handler: { _ in
                     }))
