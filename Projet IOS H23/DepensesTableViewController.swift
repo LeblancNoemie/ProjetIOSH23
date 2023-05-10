eedc34a0161d21038651f57a1ba21d95d13aacf6
Noemie Leblanc <Leblancles.noemie@gmail.com>
2023-05-10 15:02:15 -0400

    10 mai 1502

--- Projet IOS H23/DepensesTableViewController.swift
+++ Projet IOS H23/DepensesTableViewController.swift
@@ -29,6 +29,19 @@ class DepenseTableViewCell: UITableViewCell {
         depensesAPI.deleteDepense(id: depenseId)
         //Fonctionnel mais ne refresh pas la liste
         self.delegate?.refresh()
+        
+        
+        let alert = UIAlertController(title: "Sign out?", message: "You can always access your content by signing back in",         preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { _ in
+                //Cancel Action
+            }))
+        alert.addAction(UIAlertAction(title: "Sign out",
+                                          style: UIAlertAction.Style.default,
+                                          handler: {(_: UIAlertAction!) in
+                                            //Sign out action
+            }))
+        self.present(alert, animated: true)
     }
     override func awakeFromNib() {
         super.awakeFromNib()
