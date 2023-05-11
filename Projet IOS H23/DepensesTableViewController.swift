e626b39cafe74d923385d1380f753fbfb992b872
Noemie Leblanc <Leblancles.noemie@gmail.com>
2023-05-10 20:24:36 -0400

    Test 10 mai 2024

--- Projet IOS H23/DepensesTableViewController.swift
+++ Projet IOS H23/DepensesTableViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-protocol CustomTableViewDelegate{
-    func refresh()
+protocol DepenseTableViewCellDelegate:AnyObject{
+    func didButtonTap(itemId: String)
 }
 
 class DepenseTableViewCell: UITableViewCell {
     
+
     @IBOutlet weak var nom_depense: UILabel!
     @IBOutlet weak var type_paiement: UILabel!
     @IBOutlet weak var date_paiement: UILabel!
@@ -20,39 +21,21 @@ class DepenseTableViewCell: UITableViewCell {
     @IBOutlet weak var deleteDepense: UIButton!
     
     var depenseId : String = ""
-    //var table : UITableView
-    var delegate : CustomTableViewDelegate?
+    var delegate : DepenseTableViewCellDelegate?
     
-    @IBAction func deleteAction(_ sender: Any) {
-        print(depenseId)
-        let depensesAPI =  DepenseRestAPI()
-        depensesAPI.deleteDepense(id: depenseId)
-        //Fonctionnel mais ne refresh pas la liste
-        self.delegate?.refresh()
-        
-        
-        let alert = UIAlertController(title: "Sign out?", message: "You can always access your content by signing back in",         preferredStyle: UIAlertController.Style.alert)
-
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { _ in
-                //Cancel Action
-            }))
-        alert.addAction(UIAlertAction(title: "Sign out",
-                                          style: UIAlertAction.Style.default,
-                                          handler: {(_: UIAlertAction!) in
-                                            //Sign out action
-            }))
-        //self.present(alert, animated: true)
-    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    @IBAction func onDeleteClick(_ sender: Any) {
+        delegate?.didButtonTap(itemId: depenseId)
+    }
 }
 
-class DepensesTableViewController: UITableViewController, WhenDepensesReady, CustomTableViewDelegate {
-    func refresh() {
-        print("You are in CustomTableViewDelegate")
-        self.tableView.reloadData()
+class DepensesTableViewController: UITableViewController, WhenDepensesReady, DepenseTableViewCellDelegate {
+    func didButtonTap(itemId: String) {
+        print("Hello! I found this item: \(itemId)")
     }
     
     var _projectName : String = ""
@@ -76,7 +59,7 @@ class DepensesTableViewController: UITableViewController, WhenDepensesReady, Cus
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -100,56 +83,16 @@ class DepensesTableViewController: UITableViewController, WhenDepensesReady, Cus
         cell.nom_depense.text = depense.type_depense
         cell.type_paiement.text = depense.mode_paiement
         cell.depenseId = depense.id
+        //cell.delegate = self
         cell.delegate = self
         return cell
     }
+}
+
+extension ViewController : DepenseTableViewCellDelegate {
+    func didButtonTap(itemId: String) {
+        print("Item found: \(itemId)")
+    }
     
-
     
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
